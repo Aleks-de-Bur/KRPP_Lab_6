dbc3fb078b7e7ddfb50ce260d4af73465fdeafd2
Aleks-de-Bur <Aleks-de-Bur@yandex.ru>
2021-11-18 16:37:27 +0300

    Финальная версия

--- Program.cs
+++ Program.cs
@@ -6,19 +6,29 @@ namespace Lab_4
     {
         static void Main(string[] args)
         {
-            Console.Write("x=");
-            float x = float.Parse(Console.ReadLine());
-            Console.Write("y=");
-            float y = float.Parse(Console.ReadLine());
-            Console.Write("Радиус первой окружности =");
-            float r1 = float.Parse(Console.ReadLine());
-            Console.Write("Радиус второй окружности =");
-            float r2 = float.Parse(Console.ReadLine());
-            if (x * x + y * y < 9 && y > 0)
-                Console.WriteLine("внутри");
-            else if (x * x + y * y > 9 || y < 0)
-                Console.WriteLine("вне");
-            else Console.WriteLine("на границе");
+            int flag = 1;
+            do
+            {
+                Console.Write("x = ");
+                float x = float.Parse(Console.ReadLine());
+                Console.Write("y = ");
+                float y = float.Parse(Console.ReadLine());
+                Console.Write("Радиус первой окружности = ");
+                float r1 = float.Parse(Console.ReadLine());
+                Console.Write("Радиус второй окружности = ");
+                float r2 = float.Parse(Console.ReadLine());
+                if ((x * x) + (y * y) < (r1 * r1) && (x * x) + (y * y) < (r2 * r2))
+                    Console.WriteLine("Внутри");
+                else if ((x * x) + (y * y) > r1 * r1 || (x * x) + (y * y) > (r2 * r2))
+                    Console.WriteLine("Вне");
+                else Console.WriteLine("На границе");
+
+                
+                Console.WriteLine("\n\nВыберите желаемое действие:");
+                Console.WriteLine("1. Произвести рассчёты");
+                Console.WriteLine("2. Выход");
+                flag = Convert.ToInt32(Console.ReadLine());
+            } while (flag != 2);
         }
     }
 }
