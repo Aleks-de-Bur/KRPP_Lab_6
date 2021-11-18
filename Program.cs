2cb9ac39be44cde8346dd1aba624358fb5b596a2
Aleks-de-Bur <Aleks-de-Bur@yandex.ru>
2021-11-18 15:39:02 +0300

    Обновление репозитория

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Lab_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("x=");
+            float x = float.Parse(Console.ReadLine());
+            Console.Write("y=");
+            float y = float.Parse(Console.ReadLine());
+            Console.Write("Радиус первой окружности =");
+            float r1 = float.Parse(Console.ReadLine());
+            Console.Write("Радиус второй окружности =");
+            float r2 = float.Parse(Console.ReadLine());
+            if (x * x + y * y < 9 && y > 0)
+                Console.WriteLine("внутри");
+            else if (x * x + y * y > 9 || y < 0)
+                Console.WriteLine("вне");
+            else Console.WriteLine("на границе");
+        }
+    }
+}
